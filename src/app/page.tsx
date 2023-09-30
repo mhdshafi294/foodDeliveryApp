bc263b2e2c6c724cf04dde10fa80c7fd75cb908f
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-09-30 13:43:54 +0300

    made the main pAGES

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import Featured from "@/components/Featured";
+import Offer from "@/components/Offer";
+import Slider from "@/components/Slider";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World
+    <main>
+      <Slider />
+      <Featured />
+      <Offer />
     </main>
   );
 }
